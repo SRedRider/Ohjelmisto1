116ba6cd30a6bccf856ae2f4a5118fff9eae7e3b
SRedRider <130051820+SRedRider@users.noreply.github.com>
2024-08-22 11:06:23 +0300

    Update Teht2.6.py

--- mod-01/Teht2.6.py
+++ mod-01/Teht2.6.py
@@ -2,11 +2,11 @@ import random
 
 
 kolmenumeroinen_koodi = ""
-for _ in range(3):
+for i in range(3):
     kolmenumeroinen_koodi += str(random.randint(0, 9))
 
 nelinumeroinen_koodi = ""
-for _ in range(4):
+for i in range(4):
     nelinumeroinen_koodi += str(random.randint(1, 6))
 
 print(f"Kolmenumeroinen koodi: {kolmenumeroinen_koodi}")
